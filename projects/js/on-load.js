bf84b4687fb8541d16afc8a1197af35f959f3c30
JavierRizzoA <javierrizzoa@gmail.com>
2017-01-23 16:49:59 -0500

    Hide embeded flash on mobile or small windows

--- projects/js/on-load.js
+++ projects/js/on-load.js
@@ -1,9 +1,8 @@
 document.body.onload = function() {
   loadFont('https://fonts.googleapis.com/css?family=Open+Sans');
-  if(mobileAndTabletCheck()) {
-    $('.flashgame').forEach(function(el) {
-      if(window.innerWidth < el.getAttribute('data-min-res'))
-        el.style.display = 'none';
-    });
-  }
+  $('.flashgame').forEach(function(el) {
+    if(window.innerWidth < el.getAttribute('data-min-res')
+        || mobileAndTabletCheck())
+      el.style.display = 'none';
+  });
 }
--- projects/js/on-resize.js
+++ projects/js/on-resize.js
@@ -1,7 +1,6 @@
 document.body.onresize = function(event) {
-  console.log('res');
   if(!mobileAndTabletCheck()) {
-    $('flashgame').forEach(function(el) {
+    $('.flashgame').forEach(function(el) {
       if(window.innerWidth < el.getAttribute('data-min-res'))
         el.style.display = 'none';
       else
