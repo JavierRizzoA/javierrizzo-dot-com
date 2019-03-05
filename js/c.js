b25e776025e5df364ae54bcf4f1aed1ca50898de
JavierRizzoA <javierrizzoa@gmail.com>
2019-03-05 05:30:50 +0000

    move Cs

--- js/c.js
+++ js/c.js
@@ -1,3 +1,6 @@
+$('#cc')[0].innerHTML = '<p id="ck">Of course you tried <a data-opens="8">that</a>.<span data-openedby="8"> <a data-opens="9">What?</a></span><span data-openedby="9"> You expected a cool <a data-opens="10">easter egg</a>?</span><span data-openedby="10"> <a id="kf" href="#">Fine</a>.</span></p><p id="cj">Juan Ramón Peña Jr. Gutiérrez is gay.</p>';
+document.body.innerHTML += '<div id="ship"><a href="https://en.wikipedia.org/wiki/Gradius_(video_game)"><img src="img/c/ship.png" alt="Ship" /></a></div>';
+
 (function() {
 	if (typeof NodeList.prototype.forEach === "undefined") {
 		NodeList.prototype.forEach = Array.prototype.forEach;
@@ -30,4 +33,3 @@ window.onmousemove = function(e) {
 		el.style.left = e.clientX - 80 + "px";
 	});
 };
-
